0f3227d811a2f8a6626d9b490dde6440aa273bf9
Shadow <91681532+ShadowIsReal@users.noreply.github.com>
2024-03-15 14:39:05 +0000

    Major improvments
    code was overcomplicated and had useless functions

--- Source.lua
+++ Source.lua
@@ -13,24 +13,16 @@ local StatTypes = { -- table to hold different types of stats
 
 --// [[ INTERNAL FUNCTIONS ]] \\--
 
-local function ProcessStat(StatsFolder : Folder, StatTable : {}) -- function to retrieve stat type and create object with provided name and value
-	if #StatTable ~= 2 or typeof(StatTable[1]) ~= "string" then
-		warn("Invalid StatTable provided: ", StatTable)
+local function ProcessStat(StatsFolder : Folder, StatName : string, StatValue) -- function to retrieve stat type and create object with provided name and value
+	if not StatName or not StatValue then
+		warn("Invalid stat provided: ", StatName, StatValue)
 	end
 	
-	local Stat = StatTypes[typeof(StatTable[1])]()
-	Stat.Name = StatTable[1]
-	Stat.Value = StatTable[2]
+	local Stat = StatTypes[typeof(StatValue)]()
+	Stat.Name = StatName
+	Stat.Value = StatValue
 	
 	Stat.Parent = StatsFolder
-end
-
-local function ConvertToStatTable(Object) -- function to convert stat object to table
-	local StatTable = {}
-	table.insert(StatTable, Object.Name)
-	table.insert(StatTable, Object.Value)
-	
-	return StatTable
 end
 
 --// [[ MODULE FUNCTIONS ]] \\--
@@ -44,8 +36,8 @@ function module:CreateStats(Player : Player, Stats : {}) -- function to create l
 	local Leaderstats = Instance.new("Folder")
 	Leaderstats.Name = "leaderstats"
 	
-	for i=1, #Stats do
-		ProcessStat(Leaderstats, Stats[i])
+	for StatName,StatValue in Stats do
+		ProcessStat(Leaderstats, StatName, StatValue)
 	end
 	
 	Leaderstats.Parent = Player
@@ -66,22 +58,22 @@ function module:CreateHiddenStats(Player : Player, Stats : {}) -- function to cr
 	Hiddenstats.Parent = Player
 end
 
-function module:AddStat(Player : Player, StatTable : {}) -- function to create a new stat and add it to leaderstats
+function module:AddStat(Player : Player, StatName : string, StatValue) -- function to create a new stat and add it to leaderstats
 	local Leaderstats = Player:FindFirstChild("leaderstats")
 	
 	if Leaderstats then
-		ProcessStat(Leaderstats, StatTable)
+		ProcessStat(Leaderstats, StatName, StatValue)
 		return
 	end
 	
 	warn("Leaderstats not found!")
 end
 
-function module:AddHiddenStat(Player : Player, StatTable : {}) -- function to create a new stat and add it to hiddenstats
+function module:AddHiddenStat(Player : Player, StatName : string, StatValue) -- function to create a new stat and add it to hiddenstats
 	local Hiddenstats = Player:FindFirstChild("hiddenstats")
 	
 	if Hiddenstats then
-		ProcessStat(Hiddenstats, StatTable)
+		ProcessStat(Hiddenstats, StatName, StatValue)
 		return
 	end
 	
@@ -90,129 +82,78 @@ end
 
 function module:RemoveStat(Player : Player, StatName : string) -- function to remove a stat from leaderstats
 	local Leaderstats = Player:FindFirstChild("leaderstats")
+	local Hiddenstats = Player:FindFirstChild("hiddenstats")
 	
-	if Leaderstats then
-		local Stat = Leaderstats:FindFirstChild(StatName)
+	if Leaderstats or Hiddenstats then
+		local Stat = (Leaderstats:FindFirstChild(StatName) or Hiddenstats:FindFirstChild(StatName))
 		
 		if Stat then
 			Stat:Destroy()
 			return
 		end
 		
-		warn(string.format("Unable to find stat named \"%s\" in leaderstats!", StatName))
+		warn(string.format("Unable to find stat named \"%s\" in leaderstats or hiddenstats!", StatName))
 		return
 	end
 	
-	warn("Leaderstats was not found!")
+	warn("Leaderstats and hiddenstats were not found!")
 end
 	
-function module:RemoveHiddenStat(Player : Player, StatName : string) -- function to remove a stat from hiddenstats
+function module:GetStatValue(Player : Player, StatName : string) -- function to retrieve a stat value from a stat in leaderstats
+	local Leaderstats = Player:FindFirstChild("leaderstats")
 	local Hiddenstats = Player:FindFirstChild("hiddenstats")
 	
-	if Hiddenstats then
-		local Stat = Hiddenstats:FindFirstChild(StatName)
-		
-		if Stat then
-			Stat:Destroy()
-			return
-		end
-		
-		warn(string.format("Unable to find stat named \"%s\" in hiddenstats!", StatName))
-	end
-end
-	
-function module:GetStat(Player : Player, StatName : string) -- function to retrieve a stat value from a stat in leaderstats
-	local Leaderstats = Player:FindFirstChild("leaderstats")
-	
-	if Leaderstats then
-		local Stat = Leaderstats:FindFirstChild(StatName)
+	if Leaderstats or Hiddenstats then
+		local Stat = (Leaderstats:FindFirstChild(StatName) or Hiddenstats:FindFirstChild(StatName))
 		
 		if Stat then
 			return Stat.Value
 		end
 		
-		warn("Unable to find stat named %s in leaderstats!", StatName)
+		warn("Unable to find stat named %s in leaderstats or hiddenstats!", StatName)
 		return
 	end
 	
-	warn("Leaderstats was not found!")
+	warn("Leaderstats and hiddenstats were not found!")
 end
-		
-function module:GetHiddenStat(Player : Player, StatName : string) -- function to retrieve a stat value from a stat in hiddenstats
+
+function module:SetStatValue(Player : Player, StatName : string, NewValue : any) -- function to set the value of a stat in leaderstats
+	local Leaderstats = Player:FindFirstChild("leaderstats")
 	local Hiddenstats = Player:FindFirstChild("hiddenstats")
 
-	if Hiddenstats then
-		local Stat = Hiddenstats:FindFirstChild(StatName)
-
-		if Stat then
-			return Stat.Value
-		end
-
-		warn(string.format("Unable to find stat named %s in hiddenstats!", StatName))
-	end
-end
-
-function module:SetStat(Player : Player, StatName : string, NewValue : any) -- function to set the value of a stat in leaderstats
-	local Leaderstats = Player:FindFirstChild("leaderstats")
-
-	if Leaderstats then
-		local Stat = Leaderstats:FindFirstChild(StatName)
+	if Leaderstats or Hiddenstats then
+		local Stat = (Leaderstats:FindFirstChild(StatName) or Hiddenstats:FindFirstChild(StatName))
 
 		if Stat then
 			Stat.Value = NewValue
 			return
 		end
 
-		warn("Unable to find stat named %s in leaderstats!", StatName)
+		warn("Unable to find stat named %s in leaderstats or hiddenstats!", StatName)
 		return
 	end
 
-	warn("Leaderstats was not found!")
-end
-
-function module:SetHiddenStat(Player : Player, StatName : string, NewValue : any) -- function to set the value of a stat in hiddenstats
-	local Hiddenstats = Player:FindFirstChild("hiddenstats")
-
-	if Hiddenstats then
-		local Stat = Hiddenstats:FindFirstChild(StatName)
-
-		if Stat then
-			Stat.Value = NewValue
-			return
-		end
-
-		warn(string.format("Unable to find stat named %s in hiddenstats!", StatName))
-	end
+	warn("Leaderstats and hiddenstats were not found!")
 end
 
 function module:GetAllStats(Player : Player) -- function to retrieve all stats from leaderstats and convert them to a table
 	local Leaderstats = Player:FindFirstChild("leaderstats")
+	local Hiddenstats = Player:FindFirstChild("hiddenstats")
 	local Stats = {}
 	
 	if Leaderstats then
 		for _,Stat in Leaderstats:GetChildren() do
-			table.insert(Stats, ConvertToStatTable(Stat))
+			Stats[Stat.Name] = Stat.Value
 		end
-		
-		return Stats
 	end
 	
-	warn("Leaderstats was not found!")
-end
-
-function module:GetAllHidenStats(Player : Player) -- function to retrieve all stats from hiddenstats and convert them to a table
-	local Hiddenstats = Player:FindFirstChild("hiddenstats")
-	local Stats = {}
-
 	if Hiddenstats then
 		for _,Stat in Hiddenstats:GetChildren() do
-			table.insert(Stats, ConvertToStatTable(Stat))
+			Stats[Stat.Name] = Stat.Value
 		end
-
-		return Stats
 	end
-
-	warn("Hiddenstats was not found!")
+	
+	return Stats
 end
 
 return module
